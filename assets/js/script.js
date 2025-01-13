7bd688f4ec905a76d5956f6a254ffec58d747202
JeffrinJim55 <jeffrinjim55@gmail.com>
2025-01-13 13:21:42 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Add any interactive functionality for the website here (like form validation, etc.)
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website Loaded Successfully");
+});
